9682b4b2dbecf66c4c635bdd1c9a20b4317aa1e7
Ahrirg <ldargevicius20@gmail.com>
2026-03-15 13:27:25 +0200

    Get server ip from session id

--- Back_end_authority/main.py
+++ Back_end_authority/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import markdown
+import requests
 
 app = FastAPI()
 
@@ -8,8 +9,32 @@ app = FastAPI()
 def read_root():
     return {"Authority server": "Hello world"}
 
+@app.get("/join/{sessionId}")
+def get_ip_from_id(sessionId: str):
+    HOST = "http://localhost:3000" # CHANGE THIS TO BE DYNAMIC
+    if sessionId != "one": # THIS NEEDS TO BE DYNAMIC TOO
+        return {
+            "Server ip": "None",
+            "Session ID": sessionId,
+            "Error": "Server does not exist",
+        }
+
+    try:
+        r = requests.get(HOST, timeout=2)
+        return {
+            "Server ip": HOST,
+            "Session ID": sessionId
+        }
+    except requests.exceptions.RequestException:
+        return {
+            "Server ip": "None",
+            "Session ID": sessionId,
+            "Error": "Server does not exist",
+        }
+    
+
 @app.get("/info", response_class=HTMLResponse)
-def read_root():
+def read_info():
     message: str = "" 
     with open("./Info.md", "r", encoding="utf-8") as file:
         message = file.read()
