55ce73fbe05441f7f242048345cab6184384ef62
ForRealScam <kristuppaul@gmail.com>
2026-03-31 21:48:32 +0300

    added new buttons

--- Front_end/In_game/src/Leftbar.tsx
+++ Front_end/In_game/src/Leftbar.tsx
@@ -1,7 +1,13 @@
-import './Containers.css'
-import './LeftButtons.css'
+import './Css/Containers.css'
+import './Css/LeftButtons.css'
+
 import brushIcon from './assets/paint-brush.svg'
 import squareIcon from './assets/square.svg'
+import ellipseIcon from './assets/ellipse.svg'
+import triangleIcon from './assets/triangle.svg'
+import pentagonIcon from './assets/pentagon.svg'
+import starIcon from './assets/star.svg'
+import arrowIcon from './assets/up-arrow.svg'
 import { ModifyPlayerState } from '../../Canvas/player_state'
 import { ObjectType } from '../../Canvas/objects'
 
@@ -22,7 +28,16 @@ export function LeftBar({ activeTool, setActiveTool }: LeftBarProps) {
         ModifyPlayerState({selectedTool: ObjectType.Brush});
       if(toolName == "Rectangle")
         ModifyPlayerState({selectedTool: ObjectType.Rectangle});
-
+      if(toolName == "Ellipse")
+        ModifyPlayerState({selectedTool: ObjectType.Ellipse});
+      if(toolName == "Triangle")
+        ModifyPlayerState({selectedTool: ObjectType.Triangle});
+      if(toolName == "Pentagon")
+        ModifyPlayerState({selectedTool: ObjectType.Pentagon});
+      if(toolName == "Star")
+        ModifyPlayerState({selectedTool: ObjectType.Star});
+      if(toolName == "Arrow")
+        ModifyPlayerState({selectedTool: ObjectType.Arrow});
     }
   };
 
@@ -42,6 +57,46 @@ export function LeftBar({ activeTool, setActiveTool }: LeftBarProps) {
         title="Rectangle"
       >
         <img src={squareIcon} alt="Rectangle" width="35" height="35" />
+      </button>
+
+      <button 
+        className={`left-btn ${activeTool === 'Ellipse' ? 'active' : ''}`} 
+        onClick={() => handleToolClick('Ellipse')}
+        title="Ellipse"
+      >
+        <img src={ellipseIcon} alt="Ellipse" width="35" height="35" />
+      </button>
+
+      <button 
+        className={`left-btn ${activeTool === 'Triangle' ? 'active' : ''}`} 
+        onClick={() => handleToolClick('Triangle')}
+        title="Triangle"
+      >
+        <img src={triangleIcon} alt="Triangle" width="35" height="35" />
+      </button>
+
+      <button 
+        className={`left-btn ${activeTool === 'Pentagon' ? 'active' : ''}`} 
+        onClick={() => handleToolClick('Pentagon')}
+        title="Pentagon"
+      >
+        <img src={pentagonIcon} alt="Pentagon" width="35" height="35" />
+      </button>
+
+      <button 
+        className={`left-btn ${activeTool === 'Star' ? 'active' : ''}`} 
+        onClick={() => handleToolClick('Star')}
+        title="Star"
+      >
+        <img src={starIcon} alt="Star" width="35" height="35" />
+      </button>
+
+      <button 
+        className={`left-btn ${activeTool === 'Arrow' ? 'active' : ''}`} 
+        onClick={() => handleToolClick('Arrow')}
+        title="Arrow"
+      >
+        <img src={arrowIcon} alt="Arrow" width="35" height="35" />
       </button>
     </div>
   );
