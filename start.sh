39f13fb0e68bff04302b8dc3dfacf4de101d2a16
Ahrirg <ldargevicius20@gmail.com>
2026-05-21 01:59:10 +0300

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-# ./Back_end_session/target/release/Back_end_session & 
-cd ./Back_end_authority && uv run python -m fastapi run main.py --host 0.0.0.0 --port 8000 --reload
+./Back_end_session/target/release/Back_end_session &
+
+(
+  cd ./Back_end_authority && \
+  uv run python -m fastapi run main.py --host 0.0.0.0 --port 8000 --reload
+) &
+
+(
+  cd ./Back_end_database && \
+  uv run uvicorn app.main:app --reload --port 8001
+) &
+
+wait
