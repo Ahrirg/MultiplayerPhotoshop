608101c0534a20a5f7dfd562fc049cfb9707b4d0
Ahrirg <ldargevicius20@gmail.com>
2026-05-21 11:15:45 +0300

    another fix please work

--- Front_end/In_game/src/utils/websocketConnection.js
+++ Front_end/In_game/src/utils/websocketConnection.js
@@ -1,28 +1,32 @@
 export class WebsocketWrapper {
-    ipaddress;
-    websocketObject;
-    constructor(sessionIp, websocketChannel, onMessageCallback) {
-        this.ipaddress = `ws://${sessionIp.replace("http://", "")}/websockets/${websocketChannel}`;
-        this.websocketObject = new WebSocket(this.ipaddress);
-        this.websocketObject.onopen = () => {
-            console.log(`connected to ${websocketChannel} websocket`);
-        };
-        this.websocketObject.onmessage = (event) => {
-            onMessageCallback(event);
-        };
+  ipaddress;
+  websocketObject;
+  constructor(sessionIp, websocketChannel, onMessageCallback) {
+    this.ipaddress = `ws://${sessionIp.replace("http://", "")}/websockets/${websocketChannel}`;
+
+    if (this.ipaddress.includes("127.0.0.1")) {
+      const currentHost = window.location.hostname;
+      this.ipaddress = this.ipaddress.replace("127.0.0.1", currentHost);
     }
-    sendMessage(data) {
-        if (typeof data === "string") {
-            this.websocketObject.send(data);
-        }
-        else {
-            this.websocketObject.send(JSON.stringify(data));
-        }
+    this.websocketObject = new WebSocket(this.ipaddress);
+    this.websocketObject.onopen = () => {
+      console.log(`connected to ${websocketChannel} websocket`);
+    };
+    this.websocketObject.onmessage = (event) => {
+      onMessageCallback(event);
+    };
+  }
+  sendMessage(data) {
+    if (typeof data === "string") {
+      this.websocketObject.send(data);
+    } else {
+      this.websocketObject.send(JSON.stringify(data));
     }
-    getWebsocketObject() {
-        return this.websocketObject;
-    }
-    close() {
-        this.websocketObject.close();
-    }
+  }
+  getWebsocketObject() {
+    return this.websocketObject;
+  }
+  close() {
+    this.websocketObject.close();
+  }
 }
